fabccd761d0844c83765d5aa67041356453d450b
Andres Marcelo <felipemarcelo156@gmail.com>
2020-07-03 21:10:29 -0500

    Adicionando StyleSheet Para las citas

--- componentes/Cita.js
+++ componentes/Cita.js
@@ -3,23 +3,42 @@ import {Text, StyleSheet, View} from 'react-native';
 
 const Cita = ({cita}) => {
   return (
-    <View>
-      <View>
-        <Text>Paciente:</Text>
-        <Text>{cita.paciente}</Text>
+    <View style={styles.cita}>
+      <View  >
+        <Text style={styles.label} >Paciente:</Text>
+        <Text styles={styles.texto}>{cita.paciente}</Text>
       </View>
 
-      <View>
-        <Text>Propietario:</Text>
-        <Text>{cita.propietario}</Text>
+      <View >
+        <Text style={styles.label}  >Propietario:</Text>
+        <Text styles={styles.texto}>{cita.propietario}</Text>
       </View>
 
-      <View>
-        <Text>Sintomas:</Text>
-        <Text>{cita.sintomas}</Text>
+      <View  >
+        <Text style={styles.label}  > Sintomas:</Text>
+        <Text styles={styles.texto}>{cita.sintomas}</Text>
       </View>
     </View>
   );
 };
+
+const styles =StyleSheet.create({
+    cita:{
+        backgroundColor:'#FFF',
+        borderBottomColor: '#e1e1e1',
+        borderStyle:'solid',
+        borderBottomWidth: 1,
+        paddingVertical: 20,
+        paddingHorizontal: 10
+    },
+    label:{ 
+        fontWeight: 'bold',
+        fontSize:18,
+        marginTop:20
+    },
+    texto:{
+        fontSize:18,
+    }
+})
 
 export default Cita;
