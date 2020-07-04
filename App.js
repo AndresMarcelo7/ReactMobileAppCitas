331fb4ba4293d445f10fdf9c2970a3cf0dd01ed5
Andres Marcelo <felipemarcelo156@gmail.com>
2020-07-03 20:21:38 -0500

    Elementos y FlatList

--- App.js
+++ App.js
@@ -1,13 +1,39 @@
 
-import React from 'react';
+import React,{useState} from 'react';
 import {
-  Text,StyleSheet,View
+  Text,StyleSheet,View,FlatList
 } from 'react-native';
 
 const App = () => {
+  // Definir el State de Citas
+  const [citas, setCitas] = useState([
+    {id: "1", paciente:"Hook",propietario:"Juan",sintomas:"No Come"},
+    {id: "2", paciente:"Redux",propietario:"Itzel",sintomas:"No Duerme"},
+    {id: "3", paciente:"Native",propietario:"Josue",sintomas:"No Canta"}
+  ]
+  );
   return (
     <View style={styles.contenedor}>
       <Text style={styles.titulo}>Administrador de Citas</Text>
+      <FlatList
+        data={citas}
+        renderItem={ ({item}) =>(
+          <View>
+            <Text>{item.paciente}</Text>
+          </View>
+        )
+        }
+        keyExtractor={cita => cita.id}
+      />
+      
+      
+      {/*citas.map(cita => (
+        <View>
+          <Text>{cita.paciente}</Text>
+        </View>
+
+      ))*/}
+      
     </View>
   );
 };
