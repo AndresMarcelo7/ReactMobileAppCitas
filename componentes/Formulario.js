9bfb2c7c10de509eba389057f6c9e908d2894d55
Andres Marcelo <felipemarcelo156@gmail.com>
2020-07-06 19:35:49 -0500

    Adicionando TimePicker y DatePicker

--- componentes/Formulario.js
+++ componentes/Formulario.js
@@ -1,42 +1,137 @@
 import React, {useState} from 'react';
-import {Text, StyleSheet, View, TextInput} from 'react-native';
+import {Text, StyleSheet, View, TextInput, Button} from 'react-native';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
 
 const Formulario = () => {
-    return ( 
-        <>
-        <View style={styles.formulario}>
-            <View>
-                <Text style={styles.label}>Paciente:</Text>
-                <TextInput
-                    style={styles.input}
-                />
-            </View>
+  const [fecha, guardarFecha] = useState('');
+  const [hora, guardarHora] = useState('');
+
+  const [isDatePickerVisible, setDatePickerVisibility] = useState(false);
+  const [isTimePickerVisible, setTimePickerVisibility] = useState(false);
+
+  const showDatePicker = () => {
+    setDatePickerVisibility(true);
+  };
+
+  const hideDatePicker = () => {
+    setDatePickerVisibility(false);
+  };
+
+  const confirmarFecha = date => {
+    const opciones= {year:'numeric',month:'long', day: '2-digit'};
+    guardarFecha(date.toLocaleDateString('es-CO',opciones));
+    hideDatePicker();
+  };
+
+  // Muestra u oculta el time picker
+
+  const showTimePicker = () => {
+    setTimePickerVisibility(true);
+  };
+
+  const hideTimePicker = () => {
+    setTimePickerVisibility(false);
+  };
+
+  const confirmarHora = date => {
+    const opciones = { hour: '2-digit', minute: '2-digit' }
+    guardarHora(date.toLocaleTimeString('es-CO',opciones));
+    hideTimePicker();
+  };
+
+  return (
+    <>
+      <View style={styles.formulario}>
+        <View>
+          <Text style={styles.label}>Paciente:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={texto => console.log(texto)}
+          />
+        </View>
+
+        <View>
+          <Text style={styles.label}>Dueño:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={texto => console.log(texto)}
+          />
+        </View>
+
+        <View>
+          <Text style={styles.label}> Telefono Contacto:</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={texto => console.log(texto)}
+            keyboardType={'phone-pad'}
+          />
+        </View>
+        <View>
+          <Text style={styles.label}> Seleccione una Fecha</Text>
+          <Button title="Seleccionar Fecha" onPress={showDatePicker} />
+          <DateTimePickerModal
+            isVisible={isDatePickerVisible}
+            mode="date"
+            onConfirm={confirmarFecha}
+            onCancel={hideDatePicker}
+            locale="es_ES"
+            headerTextIOS="Elige La fecha"
+            cancelTextIOS="Cancelar"
+            confirmTextIOS="Confirmar"
+          />
+          <Text>{fecha}</Text>
+
 
         </View>
-            
-        </>
-    );
-}
- 
-const styles = StyleSheet.create({
-    formulario:{
-        backgroundColor: '#FFF',
-        paddingHorizontal:20,
-        paddingVertical: 10,
-        marginHorizontal:'2.5%'
-    },
-    label:{ 
-        fontWeight: 'bold',
-        fontSize:18,
-        marginTop:20
-    },
-    input:{
-        marginTop:10,
-        height:50,
-        borderColor:'#e1e1e1',
-        borderWidth: 1,
-        borderStyle: 'solid'
-    }
 
-})
+        <View>
+          <Text style={styles.label}> Seleccione una Hora</Text>
+          <Button title="Seleccionar Hora" onPress={showTimePicker} />
+          <DateTimePickerModal
+            isVisible={isTimePickerVisible}
+            mode="time"
+            onConfirm={confirmarHora}
+            onCancel={hideTimePicker}
+            locale="es_ES"
+            headerTextIOS="Elige una hora"
+            cancelTextIOS="Cancelar"
+            confirmTextIOS="Confirmar"
+            is24Hour
+          />
+          <Text>{hora}</Text>
+        </View>
+
+        <View>
+          <Text style={styles.label}> Síntomas:</Text>
+          <TextInput
+            multiline
+            style={styles.input}
+            onChangeText={texto => console.log(texto)}
+          />
+        </View>
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  formulario: {
+    backgroundColor: '#FFF',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    marginHorizontal: '2.5%',
+  },
+  label: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    marginTop: 20,
+  },
+  input: {
+    marginTop: 10,
+    height: 50,
+    borderColor: '#e1e1e1',
+    borderWidth: 1,
+    borderStyle: 'solid',
+  },
+});
 export default Formulario;
